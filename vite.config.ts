4f9e208625afe8591629648d7ebffd45134532d1
KDGT2403731 <shibakawayuya@gmail.com>
2026-02-14 14:26:12 +0900

    2026-02-14 Build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ChatApp/',
+  build: {
+    outDir: 'docs'
+  },
   test: {
     globals: true,
     environment: 'jsdom',
